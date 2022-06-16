575ba103cf93f2cf8f60bfb1f0169312a33dd7c8
gustavorafaeldev <105467235+gustavorafaeldev@users.noreply.github.com>
2022-06-15 21:27:36 -0300

    atualização

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero:'38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'01-vereador.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero:'77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url:'02-vereador.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'01-prefeito.jpg', legenda: 'Prefeito'},
+                    {url:'01-vice-prefeito.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'02-prefeito.jpg', legenda: 'Prefeiro'},
+                    {url:'02-vice-prefeito.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
